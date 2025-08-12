804b3f9a29bca6ec039a2e4eb43778a29742ffde
Abbos Ibragimov <abodeveloper2811@gmail.com>
2025-08-12 11:27:20 +0500

    fix

--- src/features/exams/pages/readings/components/ReadingQuestionContent.tsx
+++ src/features/exams/pages/readings/components/ReadingQuestionContent.tsx
@@ -33,8 +33,6 @@ const ReadingQuestionContent = ({
             ) : (
               <div>No questions available</div>
             )}
-
-            {part.questions}
           </div>
         }
       />
